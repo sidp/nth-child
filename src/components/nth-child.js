cb4281af5720e3645a84144d24e9a958a56417ae
Peter Simonsson <peter@simonsson.com>
2017-10-27 13:40:03 +0200

    First implementation of linkable patterns

--- src/components/nth-child.js
+++ src/components/nth-child.js
@@ -6,15 +6,90 @@ import Items from './items';
 
 import { blue, gray } from '../utils/styles';
 
+const DEFAULT_PATTERN = '2n+1';
+// Characters that are not useful in a nth-child pattern
+const INVALID_CHARS_REGEX = /[\(\)\\\/\<\%\€\$]/g;
+
+function allowedPattern(pattern) {
+	const ptrn = pattern.trim();
+	return ptrn.match(INVALID_CHARS_REGEX) === null;
+}
+
+function cleanPattern(pattern) {
+	return pattern.replace(INVALID_CHARS_REGEX, '');
+}
+
+function getPatternFromLocation() {
+	if (
+		typeof window !== 'undefined' &&
+		window.location.hash &&
+		window.location.hash.slice(0, 9) === '#pattern:'
+	) {
+		const pattern = window.location.hash.slice(9);
+		if (!allowedPattern(pattern)) {
+			return false;
+		}
+
+		return cleanPattern(pattern);
+	}
+
+	return false;
+}
+
 export default class NthChild extends Component {
 	state = {
-		pattern: '2n+1',
+		pattern: DEFAULT_PATTERN,
+		patternInUrl: false,
 	};
 
 	static defaultProps = {};
 
+	componentDidMount() {
+		const patternFromLocation = getPatternFromLocation();
+		if (patternFromLocation) {
+			this.setState(() => ({
+				pattern: patternFromLocation,
+				patternInUrl: true,
+			}));
+		}
+	}
+
 	handleOnChange = (pattern, callback = () => {}) => {
-		this.setState(() => ({ pattern }), callback);
+		this.setState(
+			() => ({ pattern: cleanPattern(pattern) }),
+			() => {
+				callback();
+				if (this.state.patternInUrl) {
+					this.updatePatternInUrl();
+				}
+			}
+		);
+	};
+
+	setPatternInUrl = () => {
+		this.setState(
+			() => ({
+				patternInUrl: true,
+			}),
+			this.updatePatternInUrl
+		);
+	};
+
+	updatePatternInUrl = () => {
+		if (!history.replaceState) {
+			return; // return early if there's no support for history.replaceState()
+		}
+
+		const { pattern, patternInUrl } = this.state;
+		let hash;
+
+		if (patternInUrl && allowedPattern(pattern)) {
+			hash = pattern ? `#pattern:${cleanPattern(pattern)}` : ' ';
+		} else {
+			hash = ' ';
+		}
+
+		history.replaceState(undefined, undefined, hash);
 	};
 
 	render() {
@@ -28,7 +103,14 @@ export default class NthChild extends Component {
 						onChange={this.handleOnChange}
 						tabIndex="1"
 					/>
-					)
+					) {'{'}
+					<Ellipsis>…</Ellipsis>
+					{'}'}
+					{!this.state.patternInUrl && (
+						<button onClick={this.setPatternInUrl}>
+							make url for this pattern
+						</button>
+					)}
 				</Control>
 
 				<Items numberOfItems={20} pattern={this.state.pattern} />
@@ -67,3 +149,5 @@ const StyledNumberInput = styled(NumberInput)`
 		box-shadow: inset 0 0 0 2px ${blue};
 	}
 `;
+
+const Ellipsis = styled.span`color: #e1e7ed;`;
