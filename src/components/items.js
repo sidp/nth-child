e314ffe8bba9bef6f62a8124ff78c2587c99e74b
Peter Simonsson <peter@simonsson.com>
2017-10-27 13:39:17 +0200

    Prevent characters that override the styling of the page itself

--- src/components/items.js
+++ src/components/items.js
@@ -3,12 +3,21 @@ import styled from 'styled-components';
 
 import { green, lightGray } from '../utils/styles';
 
-export default function Items({ numberOfItems, pattern }) {
+// Match characters that override the styling of the page itself
+const FORBIDDEN_CHARS_REGEX = /[\\\[\]]/g;
+function cleanPattern(pattern) {
+	return pattern.replace(FORBIDDEN_CHARS_REGEX, '');
+}
+
+export default function Items({ numberOfItems = 10, pattern = '' }) {
 	const items = Array.apply(null, { length: numberOfItems }).map((item, i) => (
 		<li key={i} />
 	));
 
-	return <StyledOrderedList pattern={pattern}>{items}</StyledOrderedList>;
+	const cleanedPattern = cleanPattern(pattern);
+	return (
+		<StyledOrderedList pattern={cleanedPattern}>{items}</StyledOrderedList>
+	);
 }
 
 const StyledOrderedList = styled.ol`
