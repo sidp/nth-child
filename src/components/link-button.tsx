2fc5f069f46635ec3553c8b6fd69fa9d6822ccf7
Peter Simonsson <peter@tulastudio.se>
2025-02-12 19:23:22 +0000

    Update link button hover effect

--- src/components/link-button.tsx
+++ src/components/link-button.tsx
@@ -12,30 +12,26 @@ export default class LinkButton extends Component<
 	LinkButtonProps,
 	LinkButtonState
 > {
-	state = {
-		hover: false,
-	};
-
-	startHover = () => {
-		this.setState(() => ({ hover: true }));
-	};
-
-	endHover = () => {
-		this.setState(() => ({ hover: false }));
-	};
-
 	render() {
 		const { children, ...props } = this.props;
-		const symbolColor = this.state.hover ? blue : gray;
 		return (
-			<button
-				className=""
-				{...props}
-				onMouseEnter={this.startHover}
-				onMouseLeave={this.endHover}
-			>
+			<button className="group" {...props}>
 				<span className="sr-only">{children}</span>
-				<SymbolLink color={symbolColor} />
+				<svg
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+				>
+					<g
+						className="stroke-gray-500 group-hover:stroke-blue-600"
+						strokeWidth="2"
+					>
+						<path d="M11.5 6.82l2.5-2.5c1.5-1.5 4-2 6 0s1.5 4.5 0 6l-4 4c-1.5 1.5-4.517 1.482-6 0-.667-.668-1-1.334-1-2" />
+						<path d="M12.82 17.619c-.668.666-1.5 1.5-2.5 2.5-1.5 1.5-4 2-6 0s-1.5-4.5 0-6l4-4c1.5-1.5 4.516-1.483 6 0 .666.666 1 1.333 1 2" />
+					</g>
+				</svg>
 			</button>
 		);
 	}
